ea2f1ac15e33f65ea9514cd632052c15ce933df7
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-01-07 09:40:44 -0500

    added functions to get random letters, numbers and special characters

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,57 @@
 // Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// Lowercase letters
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Uppercase letters
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// Numbers
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// Special characters
+function getRandomCharacter() {
+  const specialCharacter = " !\x22#$%&\x27()*+,-./:;<=>?@[\]";
+  return specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
+}
+
+const randomFunction = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomCharacter
+}
+
+
+
+
+
+
+
+
+
+/* Password criteria:
+8 - 128 characters
+Special characters: ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
+Lowercase / uppercase / numeric / special characters */
+
+// Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
